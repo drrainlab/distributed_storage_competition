24ff2ba2de714ee884636b06cf58eaaa47632da5
Gleb Bondarenko <renkornator@gmail.com>
2023-07-13 17:12:30 +0300

    comments removed

--- cmd/main.go
+++ cmd/main.go
@@ -35,28 +35,4 @@ func main() {
 
 	srv.Shutdown(ctx)
 
-	// fmt.Println(objectStorage.Nodes())
-
-	// strR := strings.NewReader(`When doneCh is closed above, the function asyncReader will return. The goroutine we created will also return the next time it evaluates the condition in the for loop. But, what if the goroutine is blocking on r.Read()? Then, we essentially have leaked a goroutine. We’re stuck until the reader unblocks.`)
-
-	// ctx := context.Background()
-	// if err := objectStorage.Store(ctx, "test", uint64(strR.Len()), strR); err != nil {
-	// 	log.Fatalf("error storing object: %v\n", err)
-	// }
-
-	// fileReader, err := objectStorage.Load(ctx, "test")
-	// if err != nil {
-	// 	log.Fatalf("error loading object: %v\n", err)
-	// }
-
-	// // Read data from multi reader
-	// b, err := io.ReadAll(fileReader)
-
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// // Optional: Verify data
-	// fmt.Println(string(b))
-
 }
